59edabe5415df0073f9039aa89c19d4fcb8baddb
Natalia K <natalia.kielbasa2002@gmail.com>
2025-06-25 21:54:59 +0200

    add base url

--- recipe-app/server.js
+++ recipe-app/server.js
@@ -11,14 +11,18 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(process.env.BASE_URL || '/', express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.json());
 
-app.use('/', recipeRoutes);
+app.use((req, res, next) => {
+  res.locals.baseUrl = process.env.BASE_URL || '/';
+  next();
+});
 
-// MongoDB database connection
+app.use(process.env.BASE_URL || '/', recipeRoutes);
+
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('MongoDB connected');
