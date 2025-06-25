2c63f08a273a81a3f0ade4dc53876a73374435fc
Natalia K <natalia.kielbasa2002@gmail.com>
2025-06-25 21:26:09 +0200

    database change

--- recipe-app/server.js
+++ recipe-app/server.js
@@ -19,16 +19,13 @@ app.use(express.json());
 app.use('/', recipeRoutes);
 
 // MongoDB database connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log('MongoDB connected');
-  app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-  });
-})
-.catch(err => {
-  console.error('MongoDB connection error:', err);
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
 });
