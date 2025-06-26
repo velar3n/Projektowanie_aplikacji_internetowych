d9616a0b831e41ed763c6f30a1894f8b84f93138
Natalia K <natalia.kielbasa2002@gmail.com>
2025-06-26 23:55:00 +0200

    commit

--- recipe-app/server.js
+++ recipe-app/server.js
@@ -20,7 +20,13 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+app.use(methodOverride((req, res) => {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    const method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
 app.use(express.json());
 
 app.use('/', recipeRoutes);
